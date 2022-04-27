34c1b3d5424edc43a47bfd7c2b08e99233899338
José Manuel <Questionsleep123@gmail.com>
2022-04-27 08:46:44 -0500

    idk

--- Manuel/count.py
+++ Manuel/count.py
@@ -1,15 +1,28 @@
 from PIL import Image
 import numpy as np
+import cv2
 import math
 
 # Open image and ensure RGB
-im = Image.open('test.png').convert('RGB')
+
+img = cv2.imread( 'iphone.png', 1 )
+
+img_tmp = cv2.resize( img, ( 0.0 ), fx = 0.3, fy = 0.3 )
+
+cv2.imwrite( 'iphone_resize.png', img_tmp )
+
+img_r = Image.open('iphone_resize.png').convert('RGB')
+
+width, height = img_r.size
 
 # Make into Numpy array
-na = np.array(im)
+na = np.array(img_r)
 
 # Get colours and corresponding counts
 colours, counts = np.unique(na.reshape(-1,3), axis=0, return_counts=1)
+
+print( "Ancho: " + str(width) + "px" )
+print( "Alto: " + str(height) + "px")
 
 print("Colores: ")
 print( colours )
@@ -17,7 +30,6 @@ print( colours )
 print("Conteo en px: ")
 print( counts[0] )
 
-mm = 0
 mm = counts[0]
 
 print( "Medida en mm²: " )
